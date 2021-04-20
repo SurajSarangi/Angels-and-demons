2609d83585387e547a3c61f0d82cd7e1264ea3c0
SurajSarangi <surajsarangi1@gmail.com>
2021-04-20 18:59:14 +0530

    fetched data from a json server

--- pages/characters.js
+++ pages/characters.js
@@ -1,11 +1,27 @@
 import styles1 from '../styles/Home.module.css';
+import styles2 from '../styles/Chars.module.css';
 import Header from '../partials/header';
 
-const Characters = () => {
+export const getStaticProps = async() => {
+    const res = await fetch('http://localhost:3003/characters');
+    const data = await res.json();
+    
+    return{
+        props : {characters : data}
+    };
+}
+
+const Characters = ({ characters }) => {
     return ( 
         <div>
             <Header title="Characters" />
             <h1 className={styles1.title}>All Characters</h1>
+            {characters.map(element => (
+                <div key={ element.id } className={styles2.desc}>
+                    <h2 className={styles2.name}>{ element.name }</h2>
+                    <p><span className={styles2.occ}>{ element.occupation }</span> at <span className={styles2.loc}>{ element.location }</span></p>
+                </div>      
+            ))}
         </div>
      );
 }
